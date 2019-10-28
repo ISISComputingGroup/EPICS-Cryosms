338065893cc525df01d5622afdf86259970a31e9
ChrisColeExpControl <christopher.cole@stfc.ac.uk>
2019-10-28 09:17:10 +0000

    some initial progress

--- CRYOSMSApp/src/CRYOSMSDriver.cpp
+++ CRYOSMSApp/src/CRYOSMSDriver.cpp
@@ -29,10 +29,12 @@
 
 #include "CRYOSMSDriver.h"
 
+#define INIT_ROW_NUM 60
+
 
 static const char *driverName = "CRYOSMSDriver"; ///< Name of driver for use in message printing 
 
-CRYOSMSDriver::CRYOSMSDriver(const char *portName, std::string devPrefix)
+CRYOSMSDriver::CRYOSMSDriver(const char *portName, std::string devPrefix, const char *tToA)
   : asynPortDriver(portName,
 	0, /* maxAddr */
 	NUM_SMS_PARAMS, /* num parameters */
@@ -45,8 +47,15 @@ CRYOSMSDriver::CRYOSMSDriver(const char *portName, std::string devPrefix)
 {
 	static const char *functionName = "asynPortDriver";
 	createParam(P_deviceNameString, asynParamOctet, &P_deviceName);
+	createParam(P_initLogicString, asynParamOctet, &P_initLogic);
+	createParam(P_rateString, asynParamOctet, &P_Rate);
+	createParam(P_maxTString, asynParamOctet, &P_MaxT);
 	createParam(P_outputModeSetString, asynParamOctet, &P_outputModeSet);
 	this->devicePrefix = devPrefix;
+	this->teslaToAmps = std::atof(tToA);
+
+	pRate_ = (epicsFloat64 *)calloc(INIT_ROW_NUM, sizeof(epicsFloat64));
+	pMaxT_ = (epicsFloat64 *)calloc(INIT_ROW_NUM, sizeof(epicsFloat64));
 	
 }
 void CRYOSMSDriver::pollerTask()
@@ -60,13 +69,54 @@ asynStatus CRYOSMSDriver::writeInt32(asynUser *pasynUser, epicsInt32 value)
 	if (function == P_outputModeSet) {
 		return putDb("HIDDEN:OUTPUTMODE:SP", &value);
 	}
+	else if (function == P_initLogic){
+		return onStart();
+	}
 	else {
 		return asynSuccess;
 	}
 }
 
+asynStatus CRYOSMSDriver::onStart()
+{
+	asynStatus status;
 
+	if (this->teslaToAmps == 0) {
+		int disVal = 1;
+		std::string statMsg = "No calibration from Tesla to Amps supplied";
+		status = putDb("STAT", &statMsg);
+		status = putDb("DISABLE", &disVal);
+		if (status != asynSuccess) {
+			return status;
+		}
+	}
+	else {
+		putDb("HIDDEN:ST", this->teslaToAmps);
+	}
+	return status;
+}
 
+asynStatus CRYOSMSDriver::procDb(std::string pvSuffix) {
+	DBADDR addr;
+	std::string fullPV = this->devicePrefix + pvSuffix;
+	if (dbNameToAddr(fullPV.c_str(), &addr)) {
+		throw std::runtime_error("PV not found: " + fullPV);
+		return asynError;
+	}
+	dbCommon *precord = addr.precord;
+	return (asynStatus)dbProcess(precord);
+}
+
+asynStatus CRYOSMSDriver::getDb(std::string pvSuffix, void *pbuffer) {
+	DBADDR addr;
+	long numReq = 1;
+	std::string fullPV = this->devicePrefix + pvSuffix;
+	if (dbNameToAddr(fullPV.c_str(), &addr)) {
+		throw std::runtime_error("PV not found: " + fullPV);
+		return asynError;
+	}
+	return (asynStatus)dbGetField(&addr, addr.dbr_field_type, &pbuffer, NULL, &numReq, NULL);
+}
 asynStatus CRYOSMSDriver::putDb(std::string pvSuffix, const void *value) {
 	DBADDR addr;
 	std::string fullPV = this->devicePrefix + pvSuffix;
@@ -78,14 +128,61 @@ asynStatus CRYOSMSDriver::putDb(std::string pvSuffix, const void *value) {
 	return (asynStatus)dbPutField(&addr, addr.dbr_field_type, value, 1);
 }
 
+asynStatus CRYOSMSDriver::readFile(const char *dir)
+{
+	//Reads PID values from a file and places them in an array
+	float rate, maxT;
+	int ind = 0;
+	FILE *fp;
+	int rowNum = 0;
+
+	if (NULL != (fp = fopen(dir, "rt"))) {
+
+		//ignore first line
+		fscanf(fp, "%*[^\n]\n");
+
+		int result = fscanf(fp, "%f %f", &rate, &maxT);
+
+		//check for incorrect format
+		if (result < 2) {
+			std::cerr << "ReadASCII: File format incorrect: " << dir << std::endl;
+			fclose(fp);
+			return asynError;
+		}
+
+		do {
+			pRate_[ind] = rate;
+			pMaxT_[ind] = maxT;
+
+			ind++;
+
+		} while (fscanf(fp, "%f %f", &rate, &maxT) != EOF && ind < INIT_ROW_NUM);
+
+		fclose(fp);
+
+		rowNum = ind;
+
+		doCallbacksFloat64Array(pRate_, rowNum, P_Rate, 0);
+		doCallbacksFloat64Array(pMaxT_, rowNum, P_MaxT, 0);
+		std::cerr << "ReadASCII: read " << rowNum << " lines from file: " << dir << std::endl;
+	}
+	else {
+		//send a file not found error
+		return asynError;
+	}
+
+	return asynSuccess;
+
+}
+
 
 extern "C"
 {
-	int CRYOSMSConfigure(const char *portName, std::string devPrefix)
+	int CRYOSMSConfigure(const char *portName, std::string devPrefix, const char *tToA)
 	{
 		try
 		{
-			new CRYOSMSDriver(portName, devPrefix);
+			new CRYOSMSDriver(portName, devPrefix, tToA);
 			return asynSuccess;
 		}
 		catch (const std::exception &ex)
@@ -96,16 +193,17 @@ extern "C"
 	}
 	// EPICS iocsh shell commands 
 
-	static const iocshArg initArg0 = { "portName", iocshArgString };			///< A name for the asyn driver instance we will create - used to refer to it from EPICS DB files
-	static const iocshArg initArg1 = { "devicePrefix", iocshArgString };
+	static const iocshArg initArg0 = { "portName", iocshArgString };			///< Port to connect to
+	static const iocshArg initArg1 = { "devicePrefix", iocshArgString };		///< PV Prefix for device
+	static const iocshArg initArg2 = { "tToA", iocshArgString };				/// Tesla - Amp calibration factor
 
-	static const iocshArg * const initArgs[] = { &initArg0, &initArg1 };
+	static const iocshArg * const initArgs[] = { &initArg0, &initArg1, &initArg2 };
 
 	static const iocshFuncDef initFuncDef = { "CRYOSMSConfigure", sizeof(initArgs) / sizeof(iocshArg*), initArgs };
 
 	static void initCallFunc(const iocshArgBuf *args)
 	{
-		CRYOSMSConfigure(args[0].sval, args[1].sval);
+		CRYOSMSConfigure(args[0].sval, args[1].sval, args[2].sval);
 	}
 
 	/// Register new commands with EPICS IOC shell
--- CRYOSMSApp/src/CRYOSMSDriver.h
+++ CRYOSMSApp/src/CRYOSMSDriver.h
@@ -5,20 +5,31 @@
 class epicsShareClass CRYOSMSDriver : public asynPortDriver
 {
 public:
-	CRYOSMSDriver(const char *portName, std::string devPrefix);
+	CRYOSMSDriver(const char *portName, std::string devPrefix, const char *tToA);
 	virtual asynStatus writeInt32(asynUser *pasynUser, epicsInt32 value);
 private:
 	std::string devicePrefix;
+	epicsFloat64 teslaToAmps;
 
 #define FIRST_SMS_PARAM P_deviceName
 
 	int P_deviceName; // string
+	int P_initLogic;
+	int P_Rate; //float
+	int P_MaxT; //float
 	int P_outputModeSet; //int as above
 
 #define LAST_SMS_PARAM 	P_outputModeSet
 #define NUM_SMS_PARAMS	(&LAST_SMS_PARAM - &FIRST_SMS_PARAM + 1)
 
+
+	epicsFloat64 *pRate_;
+	epicsFloat64 *pMaxT_;
+	asynStatus onStart();
+	asynStatus procDb(std::string pvSuffix);
+	asynStatus getDb(std::string pvSuffix, void *pbuffer);
 	asynStatus putDb(std::string pvSuffix, const void *value);
+	asynStatus readFile(const char *dir);
 	static void pollerTaskC(void* arg)
 	{
 		CRYOSMSDriver* driver = static_cast<CRYOSMSDriver*>(arg);
@@ -28,6 +39,9 @@ private:
 };
 
 #define P_deviceNameString "DEVICE"
+#define P_initLogicString "INIT_LOGIC"
+#define P_rateString "Rate"
+#define P_maxTString "MaxT"
 #define P_devicePrefixString "IN:NXHIFI:CRYOSMS_01"
 #define P_outputModeSetString "OUTPUTMODE_SET"
 
